f9bde3f5376599e42ccd79624b10743288dca0f9
Yann Wirtz <yannwzservice@gmail.com>
2026-04-19 18:15:35 +0200

    Redirects cibles : yannservice.fr -> /site-web, yannwzservice -> /urgence

--- _worker.js
+++ _worker.js
@@ -1,19 +1,18 @@
-const PRIMARY_HOST = 'yannservice.com';
-const REDIRECT_HOSTS = new Set([
-  'yannservice.fr',
-  'www.yannservice.fr',
-  'yannwzservice.com',
-  'www.yannwzservice.com',
-  'yannwzservice.fr',
-  'www.yannwzservice.fr',
-]);
+const REDIRECTS = {
+  'yannservice.fr': 'https://yannservice.com/site-web',
+  'www.yannservice.fr': 'https://yannservice.com/site-web',
+  'yannwzservice.fr': 'https://yannservice.com/urgence',
+  'www.yannwzservice.fr': 'https://yannservice.com/urgence',
+  'yannwzservice.com': 'https://yannservice.com/urgence',
+  'www.yannwzservice.com': 'https://yannservice.com/urgence',
+};
 
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
+    const target = REDIRECTS[url.hostname];
 
-    if (REDIRECT_HOSTS.has(url.hostname)) {
-      const target = `https://${PRIMARY_HOST}${url.pathname}${url.search}`;
+    if (target) {
       return Response.redirect(target, 301);
     }
 
